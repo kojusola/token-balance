be80f44d21286c2a3eeb3e5a691ab40742074784
Fafemi Adeola <adeola5678@gmail.com>
2022-08-14 03:43:43 +0100

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import Switch from 'react-switch';
+import MoonSvg from './primitives/moon';
+import SunSvg from './primitives/sun';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+export default function Header({ setThemes }: any) {
+  const handleChange = (nextChecked: boolean) => {};
+  return (
+    <div style={{ width: '100%' }}>
+      <ToastContainer />
+      <HeroContainer>
+        <HeroText>TOKENBAL</HeroText>
+        {/* <HeroButton>Connect Wallet</HeroButton> */}
+        <Switch
+          checked={false}
+          disabled={false}
+          onChange={handleChange}
+          checkedIcon={<SunSvg />}
+          uncheckedIcon={<MoonSvg />}
+          activeBoxShadow="0 0 2px 3px #917eff"
+          onHandleColor="#917eff"
+          onColor="#000000"
+          offColor="#917eff"
+        />
+      </HeroContainer>
+    </div>
+  );
+}
+
+const HeroContainer = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 40px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    padding: 20px;
+    overflow-x: hidden;
+  }
+`;
+
+const HeroText = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  font-family: 'VPPixel-Simplified';
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
