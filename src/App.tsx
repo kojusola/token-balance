57f70ebc5c883b4344d9e89e60b96058b95a4b55
Fafemi Adeola <adeola5678@gmail.com>
2022-08-14 09:38:07 +0100

    feat: add index page
    Add context wrapper also

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { AppWrapper } from './context/state';
 import './App.css';
+import Header from './components/header';
+import Hero from './components/hero';
+import Table from './components/table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <div className="App">
+        <Header />
+        <Hero />
+        <Table />
+      </div>
+    </AppWrapper>
   );
 }
 
